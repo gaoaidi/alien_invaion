b72a15a7503062f1b90bf9284a82703f1f9715b3
gaoaidi <gaoaidibenny@163.com>
2021-08-09 19:12:55 +0800

    New file:ships,settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,6 @@
 import sys
-
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -8,12 +8,14 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.settings=Settings()
 
-        self.screen=pygame.display.set_mode((1200,800))
+        self.screen=pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         
         # 设置背景色
-        self.bg_color=(230,230,230)
+        self.bg_color=(self.settings.bg_color)
     def run_game(self):
         """开始游戏的主循环"""
         while True:
